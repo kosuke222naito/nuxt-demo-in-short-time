5dadfd532a4e68e2eaab04915c92eabd7220a141
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-09-17 00:28:03 +0900

    add login page

---
+++ pages/login/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {
+  NButton,
+  NForm,
+  NFormItem,
+  NInput,
+} from "naive-ui";
+
+definePageMeta({
+  layout: "static",
+});
+</script>
+
+<template>
+  <NForm>
+    <div class="flex flex-col gap-[24px]">
+      <NFormItem label="ID" path="id">
+        <NInput />
+      </NFormItem>
+      <NFormItem label="パスワード" path="password">
+        <NInput :type="'password'" />
+      </NFormItem>
+      <NButton :attr-type="'submit'">
+        <span class="text-[14px]">ログイン</span>
+      </NButton>
+    </div>
+  </NForm>
+</template>
