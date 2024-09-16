3a7993b1816c3db896690442dc9356b6017c80e2
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-09-16 23:40:47 +0900

    add posts

---
+++ pages/posts/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { NDataTable, type DataTableColumns } from "naive-ui";
+import type { Post } from "~/models/post";
+
+const { data: posts } = useFetch<Post[]>(
+  "https://jsonplaceholder.typicode.com/posts"
+);
+
+const columns = computed<DataTableColumns<Post>>(() => [
+  {
+    key: "id",
+    title: "ID",
+    width: 60,
+    render: row => row.id,
+  },
+  {
+    key: "title",
+    title: "Title",
+    render: row => row.title,
+  },
+  {
+    key: "body",
+    title: "Body",
+    render: row => row.body,
+  },
+]);
+</script>
+
+
+<template>
+  <div class="px-[40px] py-[32px] flex flex-col gap-[32px]">
+    <span class="text-[24px] font-bold">Posts</span>
+    <NDataTable remote v-if="posts" :data="posts" :columns />
+  </div>
+</template>
