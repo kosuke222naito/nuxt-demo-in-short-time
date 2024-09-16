aafdd9c542bcc6ba0cd55fad668ddfcea24082c0
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-09-16 12:27:42 +0900

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: false,
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+      tailwindcss: {},
+    }
+  },
+  css: ["@/assets/styles/main.scss"],
 })
