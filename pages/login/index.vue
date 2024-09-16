f74f497ba40839fa6fda0f387e37d188cc25e5bd
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-09-17 00:30:25 +0900

    add form validation

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -6,19 +6,44 @@ import {
   NInput,
 } from "naive-ui";
 
+import type { FormInst, FormRules, } from "naive-ui";
+
 definePageMeta({
   layout: "static",
 });
+
+interface FormValue {
+  id: string;
+  password: string;
+}
+const emptyFormValue: FormValue = {
+  id: "",
+  password: "",
+};
+
+const formValue = ref<FormValue>(emptyFormValue);
+const formRef = ref<FormInst>();
+
+const rules: FormRules = {
+  id: {
+    required: true,
+    message: "IDを入力してください",
+  },
+  password: {
+    required: true,
+    message: "パスワードを入力してください",
+  },
+};
 </script>
 
 <template>
-  <NForm>
+  <NForm ref="formRef" :rules="rules" :model="formValue">
     <div class="flex flex-col gap-[24px]">
       <NFormItem label="ID" path="id">
-        <NInput />
+        <NInput v-model:value="formValue.id" />
       </NFormItem>
       <NFormItem label="パスワード" path="password">
-        <NInput :type="'password'" />
+        <NInput v-model:value="formValue.password" :type="'password'" />
       </NFormItem>
       <NButton :attr-type="'submit'">
         <span class="text-[14px]">ログイン</span>
