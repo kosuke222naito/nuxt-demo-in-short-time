72bac0c5d2094aa335454cc72c4181c5b9a9df5e
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-09-16 14:16:19 +0900

    add default layout

---
+++ layouts/defautl.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {
+  NButton,
+  NLayout,
+  NLayoutContent,
+  NLayoutHeader,
+  NLayoutSider,
+  NMenu,
+  type MenuOption,
+} from "naive-ui";
+
+const menuOptions: MenuOption[] = [
+  {
+    key: "users",
+    label: "Users",
+  },
+  {
+    key: "posts",
+    label: "Posts",
+  },
+];
+</script>
+
+<template>
+  <NLayout has-slider class="min-h-screen">
+    <NLayoutSider bordered class="min-h-screen">
+      <NMenu :options="menuOptions" />
+    </NLayoutSider>
+    <NLayout class="min-h-screen">
+      <NLayoutHeader>
+        <div class="py-[24px] px-[40px] flex flex-row items-center justify-end">
+          <div class="flex flex-row items-center gap-[12px]">
+            <span class="text-[14px]">hoge</span>
+            <NButton>
+              <span class="text-[14px]">ログアウト</span>
+            </NButton>
+          </div>
+        </div>
+      </NLayoutHeader>
+      <NLayoutContent>
+        <slot />
+      </NLayoutContent>
+    </NLayout>
+  </NLayout>
+</template>
