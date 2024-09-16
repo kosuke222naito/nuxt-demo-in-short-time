942371b31e89db073fb0da6e4e0c8bbd8a4d658f
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-09-16 14:04:59 +0900

    use usetFetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <span>Hello World! from Nuxt</span>
+    <pre>{{ JSON.stringify(users, null, 2) }}</pre>
   </div>
 </template>
+
+<script setup lang="ts">
+import type { User } from "~/models/user";
+
+const { data: users } = useFetch<User[]>(
+  "https://jsonplaceholder.typicode.com/users"
+);
+</script>
